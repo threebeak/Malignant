1ee3e906fc377d0d37894537c3f79c960588c6ff
Steve Nelson <stephennelson1@charter.net>
2022-10-23 15:27:20 -0500

    Init
    Initial Commit

---
+++ Source/Malignant/Malignant.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Malignant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Malignant, "Malignant" );
