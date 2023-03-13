2e5b7915e6ca4da9608bc9437252da82d15f2643
Steve Nelson <stephennelson1@charter.net>
2023-03-13 14:05:26 -0500

    Item base classes
    Added ItemBase, MutagenBase, and CompoundBase classes for start of new mixing table

---
+++ Source/Malignant/Public/CompoundBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemBase.h"
+#include "CompoundBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MALIGNANT_API UCompoundBase : public UItemBase
+{
+	GENERATED_BODY()
+	
+};
